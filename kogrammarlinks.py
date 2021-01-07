ccf078306c4d64249c21eb9ebcbba43b98d6daa5
danjclegg <danjclegg@gmail.com>
2021-01-07 14:07:53 -0800

    grammar additions

--- kogrammarlinks.py
+++ kogrammarlinks.py
@@ -24,40 +24,48 @@ class KoGrammarLinks:
 
         #supplementary list of basic particles
         self.supplementary_grammar_links = [
-            ('~은', 'Subject/Topic', '9DK', 'https://www.90daykorean.com/korean-particles/#-eun-neun-subjecttopic'),
-            ('~는', 'Subject/Topic', '9DK', 'https://www.90daykorean.com/korean-particles/#-eun-neun-subjecttopic'),
-            ('~이', 'Subject', '9DK', 'https://www.90daykorean.com/korean-particles/#-i-ga-subject'),
-            ('~가', 'Subject', '9DK', 'https://www.90daykorean.com/korean-particles/#-i-ga-subject'),
-            ('~을', 'Object', '9DK', 'https://www.90daykorean.com/korean-particles/#-eulreul-object'),
-            ('~를', 'Object', '9DK', 'https://www.90daykorean.com/korean-particles/#-eulreul-object'),
-            ('~에', 'Time/Location', '9DK', 'https://www.90daykorean.com/korean-particles/#-e-timelocation'),
-            ('~에서', 'Location', '9DK', 'https://www.90daykorean.com/korean-particles/#-eseo-location'),
-            ('~께', 'To give', '9DK', 'https://www.90daykorean.com/korean-particles/#-kkeegehante-to-give-someone-something'),
-            ('~에게', 'To give', '9DK', 'https://www.90daykorean.com/korean-particles/#-kkeegehante-to-give-someone-something'),
-            ('~한테', 'To give', '9DK', 'https://www.90daykorean.com/korean-particles/#-kkeegehante-to-give-someone-something'),
-            ('~께서', 'To receive', '9DK', 'https://www.90daykorean.com/korean-particles/#-kkeseoeseohanteseo-to-receive-something-from-someone'),
-            ('~에서', 'To receive', '9DK', 'https://www.90daykorean.com/korean-particles/#-kkeseoeseohanteseo-to-receive-something-from-someone'),
-            ('~한테서', 'To receive', '9DK', 'https://www.90daykorean.com/korean-particles/#-kkeseoeseohanteseo-to-receive-something-from-someone'),
-            ('~도', 'Also', '9DK', 'https://www.90daykorean.com/korean-particles/#-do-also'),
-            ('~(으)로', 'Direction etc.', '9DK', 'https://www.90daykorean.com/korean-particles/#-euroro-direction-and-multiple-other-meanings'),
-            ('~부터', 'Start', '9DK', 'https://www.90daykorean.com/korean-particles/#-buteo-start'),
-            ('~까지', 'Until', '9DK', 'https://www.90daykorean.com/korean-particles/#-kkaji-until'),
-            ('~들', 'Plural', '9DK', 'https://www.90daykorean.com/korean-particles/#-deul-plural'),
-            ('~만', 'Only', '9DK', 'https://www.90daykorean.com/korean-particles/#-man-only'),
-            ('~의', 'Possessive/\'s', '9DK', 'https://www.90daykorean.com/korean-particles/#-ui-possessive'),
+            ('~은', 'subject/topic', '9DK', 'https://www.90daykorean.com/korean-particles/#-eun-neun-subjecttopic'),
+            ('~는', 'subject/topic', '9DK', 'https://www.90daykorean.com/korean-particles/#-eun-neun-subjecttopic'),
+            ('~이', 'subject', '9DK', 'https://www.90daykorean.com/korean-particles/#-i-ga-subject'),
+            ('~가', 'subject', '9DK', 'https://www.90daykorean.com/korean-particles/#-i-ga-subject'),
+            ('~을', 'object', '9DK', 'https://www.90daykorean.com/korean-particles/#-eulreul-object'),
+            ('~를', 'object', '9DK', 'https://www.90daykorean.com/korean-particles/#-eulreul-object'),
+            ('~에', 'time/location', '9DK', 'https://www.90daykorean.com/korean-particles/#-e-timelocation'),
+            ('~에서', 'location', '9DK', 'https://www.90daykorean.com/korean-particles/#-eseo-location'),
+            ('~께', 'to give', '9DK', 'https://www.90daykorean.com/korean-particles/#-kkeegehante-to-give-someone-something'),
+            ('~에게', 'to give', '9DK', 'https://www.90daykorean.com/korean-particles/#-kkeegehante-to-give-someone-something'),
+            ('~한테', 'to give', '9DK', 'https://www.90daykorean.com/korean-particles/#-kkeegehante-to-give-someone-something'),
+            ('~께서', 'to receive', '9DK', 'https://www.90daykorean.com/korean-particles/#-kkeseoeseohanteseo-to-receive-something-from-someone'),
+            ('~에서', 'to receive', '9DK', 'https://www.90daykorean.com/korean-particles/#-kkeseoeseohanteseo-to-receive-something-from-someone'),
+            ('~한테서', 'to receive', '9DK', 'https://www.90daykorean.com/korean-particles/#-kkeseoeseohanteseo-to-receive-something-from-someone'),
+            ('~도', 'also', '9DK', 'https://www.90daykorean.com/korean-particles/#-do-also'),
+            ('~(으)로', 'direction etc.', '9DK', 'https://www.90daykorean.com/korean-particles/#-euroro-direction-and-multiple-other-meanings'),
+            ('~부터', 'start', '9DK', 'https://www.90daykorean.com/korean-particles/#-buteo-start'),
+            ('~까지', 'until', '9DK', 'https://www.90daykorean.com/korean-particles/#-kkaji-until'),
+            ('~들', 'plural', '9DK', 'https://www.90daykorean.com/korean-particles/#-deul-plural'),
+            ('~만', 'only', '9DK', 'https://www.90daykorean.com/korean-particles/#-man-only'),
+            ('~의', 'possessive/\'s', '9DK', 'https://www.90daykorean.com/korean-particles/#-ui-possessive'),
             ('~과', 'and/with/as with', '9DK', 'https://www.90daykorean.com/korean-particles/#-gwawa-andwithas-with'),
             ('~와', 'and/with/as with', '9DK', 'https://www.90daykorean.com/korean-particles/#-gwawa-andwithas-with'),
             ('~(이)랑', 'and/with/as with', '9DK', 'https://www.90daykorean.com/korean-particles/#-irangrang-andwithas-with'),
             ('~하고', 'and/with/as with', '9DK', 'https://www.90daykorean.com/korean-particles/#-hago-andwithas-with'),
             ('~고', 'connective', '9DK', 'https://www.90daykorean.com/korean-particles/#-go-connective'),
             ('-(으)면', 'if/when -, then', 'DAN', '#'),
-            ('-던', '- was', 'DAN', '#'),
             ('-에게', 'to/w.r.t. -', 'DAN', '#'),
             ('~(으)며', 'while', 'STK', 'https://korean.stackexchange.com/questions/2628/when-would-i-use-%EB%A9%B4%EC%84%9C-vs-%EB%A9%B0'),
             ('~(으)면서', 'concurrently', 'STK', 'https://korean.stackexchange.com/questions/2628/when-would-i-use-%EB%A9%B4%EC%84%9C-vs-%EB%A9%B0'),
             ('-서', 'so/so as to', 'HAD', 'https://hanguladay.com/2009/01/24/verbs-in-infinitive-and-particle-%EC%84%9C/'),
             ('-(으)세', 'lets -', 'KGF', 'http://koreangrammaticalforms.com/entry.php?eid=0000001335'),
-            ('-(이)라는', '-called', '1LK', 'https://123learnkorean.wordpress.com/2009/08/25/%EC%9D%B4%EB%9D%BC%EB%8A%94-called/')
+            ('-(이)라는', '-called', '1LK', 'https://123learnkorean.wordpress.com/2009/08/25/%EC%9D%B4%EB%9D%BC%EB%8A%94-called/'),
+            ('-게', 'so that -, - caused by', 'HSK', 'https://www.howtostudykorean.com/unit-3-intermediate-korean-grammar/unit-3-lessons-51-58/lesson-56/#56new'),
+            ('-자', 'let\'s -', 'HSK', 'https://www.howtostudykorean.com/unit-2-lower-intermediate-korean-grammar/unit-2-lessons-42-50/lesson-44/#441'),
+            #next one should match both
+            #('-읍시다', 'let\'s -', 'HSK', 'https://www.howtostudykorean.com/unit-2-lower-intermediate-korean-grammar/unit-2-lessons-42-50/lesson-44/#441'),
+            ('-ㅂ시다', 'let\'s -', 'HSK', 'https://www.howtostudykorean.com/unit-2-lower-intermediate-korean-grammar/unit-2-lessons-42-50/lesson-44/#441'),
+            ('-던', 'past experience -', 'HSK', 'https://www.howtostudykorean.com/unit-2-lower-intermediate-korean-grammar/unit-2-lessons-26-33/lesson-27/#271'),
+            ('-았던', 'past experience -', 'HSK', 'https://www.howtostudykorean.com/unit-2-lower-intermediate-korean-grammar/unit-2-lessons-26-33/lesson-27/#271'),
+            ('-었던', 'past experience -', 'HSK', 'https://www.howtostudykorean.com/unit-2-lower-intermediate-korean-grammar/unit-2-lessons-26-33/lesson-27/#271'),
+            ('-은', 'past -', 'HSK', 'https://www.howtostudykorean.com/unit-2-lower-intermediate-korean-grammar/unit-2-lessons-26-33/lesson-27/#271')
         ]
 
     #to be used manually only to cache links for lookup by rest of this class
@@ -96,7 +104,7 @@ class KoGrammarLinks:
         matches = []
         for key in self.grammarlinks:
             if re.match(pattern, key) is not None:
-                matches.append(("KGF: " + key, "http://koreangrammaticalforms.com/entry.php?eid=" + html.escape(self.grammarlinks[key])))
+                matches.append((key, "http://koreangrammaticalforms.com/entry.php?eid=" + html.escape(self.grammarlinks[key])))
         return matches
 
 
@@ -104,7 +112,7 @@ class KoGrammarLinks:
         matches = []
         for key, definition, source, link in self.supplementary_grammar_links:
             if re.match(pattern, key) is not None:
-                matches.append((source + ": " + key + " " + definition, link))
+                matches.append((key + " " + definition, link))
         return matches
 
 
@@ -136,7 +144,7 @@ class KoGrammarLinks:
                         preceeding_char = ""
                 
                 # if we're truncating, then skip any obvious endings
-                if trunc and particle == "요" or particle == "은" or particle == "을" or particle == "는" or particle == "를" or particle == "다":
+                if trunc and (particle == "요" or particle == "은" or particle == "을" or particle == "는" or particle == "를" or particle == "다"):
                     continue
                 
                 #get final element of that char
@@ -209,7 +217,7 @@ class KoGrammarLinks:
                 grammarlinks.extend(self.matches_search(pattern))
                 #print(len(grammarlinks))
                 
-                #if particle == "에서":
+                ##if particle == "은":
                 #print("branch i; particle, preceeding_char, preceeding_char_final, hit_a_symbol, last_morph_index, remaining_morphs, nextmorph_text, nextbranch")
                 #print(str(branch) + " " + str(i) + "\t" + str(particle) + "\t" + str(preceeding_char) + "\t" + str(preceeding_char_final) + "\t" + str(hit_a_symbol) + "\t'" + str(last_morph_index) + "'\t'" + str(remaining_morphs) + "'\t'" + str(nextmorph_text) + "'\t" + str(nextbranch))
                     
